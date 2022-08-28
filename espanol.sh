8fe5207adf693eece2a3ea310b640dc62cd64e36
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-28 12:31:06 -0300

    Update espanol.sh

--- espanol.sh
+++ espanol.sh
@@ -2,6 +2,8 @@
 
 # ESPANHA - update the streams
 
+$(wget https://raw.githubusercontent.com/davidmuma/log_dobleM/master/TDTChannels.m3u
+
 sed -e "pipe:///usr/bin/env streamlink --stdout --default-stream best --url $(wget https://raw.githubusercontent.com/davidmuma/log_dobleM/master/TDTChannels.m3u )/" ESPANOL.M3U
 
 exit 0
