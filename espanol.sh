8fb474391d9101008e1686e0a4e18fa88505bd56
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-28 12:37:35 -0300

    Update espanol.sh

--- espanol.sh
+++ espanol.sh
@@ -4,6 +4,6 @@
 
 $resultado = wget http://www.atp.com.br/extranet/servicoBanco/service.asmx
 
-sed -e "pipe:///usr/bin/env streamlink --stdout --default-stream best --url $(wget https://raw.githubusercontent.com/davidmuma/log_dobleM/master/TDTChannels.m3u -O ESPANOL.M3U )/"
+sed -e "pipe:///usr/bin/env streamlink --stdout --default-stream best --url $(wget -r https://raw.githubusercontent.com/davidmuma/log_dobleM/master/TDTChannels.m3u -O ESPANOL.M3U )/"
 
 exit 0
