2f72a4d1bcc37c0796c8517c3961ff8625a1ac35
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-28 12:41:28 -0300

    Delete espanol.sh

--- espanol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ESPANHA - update the streams
-
-$resultado = wget http://www.atp.com.br/extranet/servicoBanco/service.asmx
-
-sed -e "pipe:///usr/bin/env streamlink --stdout --default-stream best --url $(wget -r https://raw.githubusercontent.com/davidmuma/log_dobleM/master/TDTChannels.m3u -O ESPANOL.M3U )/"
-
-exit 0
