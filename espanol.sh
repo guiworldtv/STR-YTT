cd5ba088265877dba34ac819e8ee4faa8193c59c
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-28 12:21:32 -0300

    Add files via upload

---
+++ espanol.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ESPANHA - update the streams
+
+sed -e "pipe:///usr/bin/env streamlink --stdout --default-stream best --url $(wget https://raw.githubusercontent.com/davidmuma/log_dobleM/master/TDTChannels.m3u )/" ESPANOL.M3U
+
+exit 0
