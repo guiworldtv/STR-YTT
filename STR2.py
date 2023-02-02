2c355776e2d7be5d5c91f5d34ba3bf42b737be90
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-02 03:50:05 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -18,6 +18,8 @@ for i in range(1, 3):
     video_titles = [item.text for item in soup.find_all("span", class_="item-title")]
     video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", class_="item")]
     Data = [item.text for item in soup.find_all("span", class_="item-date")]
+    image_url = item["style"].split("url(")[1].split(")")[0]
+
 
 
 for title, link in zip(video_titles, video_links):
