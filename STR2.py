fc0ec3cd27760ff36af93d7b958627b03aec68a4
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-02 02:29:50 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -23,6 +23,7 @@ for i in range(1, 3):
         now = datetime.datetime.now()
         timestamp = now.strftime("%m%d%H%M%S")
         video_url = streamlink.streams(link)["best"].url
-        m3u8_file.write(f"#EXTINF:-1 group-title = "{title}",{title}\n{video_url}\n")
+        m3u8_file.write(f"#EXTINF:-1 group-title = "{title}",
+        {title}\n{video_url}\n")
 
 m3u8_file.close()
