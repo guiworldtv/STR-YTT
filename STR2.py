e2abf4e0123ea84979721373e98d1a40a40ae77c
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-02 02:14:01 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -23,6 +23,6 @@ for i in range(1, 3):
         now = datetime.datetime.now()
         timestamp = now.strftime("%m%d%H%M%S")
         video_url = streamlink.streams(link)["best"].url
-        m3u8_file.write(f"{title}{Data}{timestamp}\n{video_url}\n")
+        m3u8_file.write(f"{title}{Date}{timestamp}\n{video_url}\n")
 
 m3u8_file.close()
