20006602287df8a958b0535481974c4d4566fe55
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-02 06:51:21 -0300

    Update CHILE.sh

--- CHILE.sh
+++ CHILE.sh
@@ -6,6 +6,6 @@ python -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python CHILE.py "190.107.224.150:3128" > ../CHILE.m3u
+python CHILE.py "190.107.224.150:3128" > ../lista4chile.m3u
 
 echo m3u grabbed
