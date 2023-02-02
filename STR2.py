20996499df120172c575254e08f725c8d9ec2d4e
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-02 02:11:41 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -19,7 +19,7 @@ for i in range(1, 3):
     video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", class_="item")]
     Data = [item.text for item in soup.find_all("span", class_="item-date")]
 
-    for title, data, link in zip(video_titles, video_links):
+    for title, date, link in zip(video_titles, video_links):
         now = datetime.datetime.now()
         timestamp = now.strftime("%m%d%H%M%S")
         video_url = streamlink.streams(link)["best"].url
