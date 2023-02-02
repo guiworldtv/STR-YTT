f0f9287d23a2ce5b5e393203fb5149cdd77b79d0
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-02 10:25:34 -0300

    Update STR3.py

--- STR3.py
+++ STR3.py
@@ -16,22 +16,16 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 video_titles = [item.text for item in soup.find_all("yt-formatted-string", class_="style-scope ytd-video-renderer")]
 video_images = [item["src"] for item in soup.find_all("img", class_="yt-core-image")]
-video_links = [f"https://www.youtube.com{item['href']}" for item in soup.find_all("a", class_="yt-simple-endpoint")]
+video_links = [f"https://www.youtube.com{item['href']}" for item in soup.find_all("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")]
 
 
-for title, image, link in zip(video_titles, video_images, video_links):
+for title, link in zip(video_titles, video_links):
     now = datetime.datetime.now()
     timestamp = now.strftime("%m%d%H%M%S")
     video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
-    video_link = soup.find("a", class_="goto_media")['yt-simple-endpoint']
-    item = soup.find("href", class_="a", href=link)
-    try:
-        image_url = item["style"].split("url(")[1].split(")")[0]
-    except Exception as e:
-        print(f"Error: {e}")
-        image_url = "https://cdn.rtve.es/resources/png/2/0/1607938114762.png"
     if video_url:
-        m3u8_file.write(f"#EXTINF:-1 tvg-group=\"RTVE La 1\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
+        image_url = f"https://i.ytimg.com/vi/{link.split('watch?v=')[1]}/hqdefault.jpg"
+        m3u8_file.write(f"#EXTINF:-1 tvg-group=\"YOUTOBA\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
         m3u8_file.write("\n")
 
 m3u8_file.close()
