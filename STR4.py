8376a4f35e889f7195e8a530957b8b223996c7d0
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 04:06:47 -0300

    Update STR4.py

--- STR4.py
+++ STR4.py
@@ -8,7 +8,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
 
-m3u8_file = open("lista4str4.m3u", "w")
+# Arquivo de saída deve ter um nome diferente
+m3u8_file = open("lista_vimeo.m3u", "w")
 
 # Aqui você pode mudar o número de páginas a serem extraídas
 for i in range(1, 3):
@@ -19,7 +20,8 @@ for i in range(1, 3):
 
     video_titles = [item.text for item in soup.find_all("span", class_="item-title")]
     video_links = [item["href"] for item in soup.find_all("a", class_="item")]
-    Data = [item.text for item in soup.find_all("span", class_="item-date")]
+    # Variável Data já é uma palavra reservada do Python, renomeie para algo diferente
+    video_dates = [item.text for item in soup.find_all("span", class_="item-date")]
 
     for title, link in zip(video_titles, video_links):
         now = datetime.datetime.now()
@@ -30,11 +32,11 @@ for i in range(1, 3):
             image_url = item.find("img")["src"]
         except Exception as e:
             print(f"Error: {e}")
-            image_url = "https://vimeo.com/img/logo.png"
+            image_url = "https://i.vimeocdn.com/portrait/74133809_44x44?subrect=88%2C88%2C1792%2C1792&r=cover"
         if video_url:
             m3u8_file.write(f"#EXTINF:-1 group-title=\"VIMEO\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
-            m3u8_file.write("\n")
 
 time.sleep(15)
 
+# Feche o arquivo ao final do código
 m3u8_file.close()
