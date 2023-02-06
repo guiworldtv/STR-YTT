41bdec8230efb2664f52d0a96fd283287f5ac716
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 03:40:43 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -12,8 +12,13 @@ names = []
 images = []
 
 for link in soup.find_all('a', href=True):
-    if '/direto/TVI' in link['href']:
-        links.append('https://tviplayer.iol.pt' + link['href'])
+    if link['href'].startswith('/direto/'):
+        video_url = 'https://tviplayer.iol.pt' + link['href']
+        title = link.find_next('div', class_='titlePrograma').text
+        image_url = link.find_next('div', class_='programCover')['style'].split("url(")[1].split(")")[0]
+        entry = "#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{}\",{}\n{}".format(image_url, title, video_url)
+        with open('tvi_player.m3u', 'a') as file:
+            file.write(entry)
 
 for name in soup.find_all('div', class_='titlePrograma'):
     names.append(name.text)
