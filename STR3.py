ed9732dd24c35582a7b34684d79a2a170cee8d3a
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 03:59:06 -0300

    Update STR3.py

--- STR3.py
+++ STR3.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import datetime
 import streamlink
 
+
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
@@ -23,9 +25,13 @@ for title, link in zip(video_titles, video_links):
     now = datetime.datetime.now()
     timestamp = now.strftime("%m%d%H%M%S")
     video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
-    if video_url:
-        image_url = f"https://i.ytimg.com/vi/{link.split('watch?v=')[1]}/hqdefault.jpg"
-        m3u8_file.write(f"#EXTINF:-1 tvg-group=\"YOUTOBA\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
-        m3u8_file.write("\n")
+if video_url:
+    image_url = f"https://i.ytimg.com/vi/{link.split('watch?v=')[1]}/hqdefault.jpg"
+    m3u8_file.write(f"#EXTINF:-1 tvg-group=\"YOUTOBA\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
+    m3u8_file.write("\n")
+else:
+    print(f"Não foi possível acessar o vídeo: {title}")
+
+    
 
 m3u8_file.close()
