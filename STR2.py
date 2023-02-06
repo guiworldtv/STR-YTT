e94b7d3db6a5130b2b75bc21c43f3e80e0ec90fc
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 03:02:17 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -1,79 +1,55 @@
 import os
 import subprocess
 import sys
+import logging
+
+logger = logging.getLogger(__name__)
 
 def run_command(command):
-    """
-    Executa um comando e retorna a saída e o erro.
-    """
-    try:
-        process = subprocess.Popen(
-            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        output, error = process.communicate()
-        return output.decode().strip(), error.decode().strip()
-    except Exception as e:
-        print("Erro ao executar comando:", e)
-        sys.exit(1)
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    output, error = process.communicate()
+    if process.returncode != 0:
+        logger.error(f"Error running command: {command}. Error: {error}")
+        return None, None
+    return output.decode().strip(), error.decode().strip()
 
-def install_dependency(dependency_name, dependency_url, binary_path):
-    """
-    Instala uma dependência a partir de uma URL e define permissões de execução.
-    """
-    try:
-        os.system(f"wget {dependency_url} -O {binary_path}")
-        os.system(f"chmod a+rx {binary_path}")
-    except Exception as e:
-        print(f"Erro ao instalar {dependency_name}:", e)
-        sys.exit(1)
+def install_dependencies():
+    dependencies = ["yt-dlp", "youtube-dl", "streamlink"]
+    for dependency in dependencies:
+        if not run_command(f"which {dependency}")[0]:
+            install_command = f"pip install {dependency}"
+            os.system(install_command)
 
-def install_yt_dlp():
-    """
-    Instala o yt-dlp.
-    """
-    install_dependency("yt-dlp", "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp", "/usr/local/bin/yt-dlp")
+def write_m3u_header(f):
+    f.write("#EXTM3U\n")
+    f.write("#EXT-X-VERSION:3\n")
 
-def install_youtube_dl():
-    """
-    Instala o youtube-dl.
-    """
-    install_dependency("youtube-dl", "https://yt-dl.org/downloads/latest/youtube-dl", "/usr/local/bin/youtube-dl")
-    try:
-        os.system("youtube-dl -U")
-    except Exception as e:
-        print("Erro ao atualizar youtube-dl:", e)
-        sys.exit(1)
-
-def install_streamlink():
-    """
-    Instala o Streamlink usando o pip.
-    """
-    try:
-        os.system("pip install --user --upgrade streamlink")
-    except Exception as e:
-        print("Erro ao instalar streamlink:", e)
-        sys.exit(1)
+def write_m3u_entry(f, tvg_id, tvg_logo, title, stream_url):
+    f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n")
+    f.write(f'#EXTINF:-1 tvg-id="{tvg_id}" tvg-logo="{tvg_logo}",{title}\n')
+    f.write(stream_url + "\n")
 
 def get_lista4_m3u8():
-    with open("./BLINK182.m3u", "w") as f:
-        f.write("#EXTM3U\n")
-        f.write("#EXT-X-VERSION:3\n")
-        f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n")
-        f.write('#EXTINF:-1 tvg-id="$(yt-dlp --get-title https://www.youtube.com/watch?v=EeQnkxY9QFs)" group-title="Reality Show\'S Live" group-title="CNN\'s" tvg-logo="$(yt-dlp --get-thumbnail https://www.youtube.com/watch?v=EeQnkxY9QFs)", YOUTUBEAOVIVO - $(yt-dlp --get-title https://www.youtube.com/watch?v=EeQnkxY9QFs)\n')
-        f.write("$(streamlink --url --default-stream  --stream-url  https://www.youtube.com/@TelemundoEntretenimiento/live best)\n")
-        f.write("#EXTM3U\n")
-        f.write("#EXT-X-VERSION:3\n")
-        f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n")
-        f.write('#EXTINF:-1 tvg-id="$(youtube-dl --get-id https://www.dailymotion.com/video/x82pp99)" tvg-logo="$(youtube-dl --get-thumbnail https://www.dailymotion.com/video/x82pp99)",$(youtube-dl -e -C https://www.dailymotion.com/video/x82pp99) TV BRASIL\n')
-        f.write("$(streamlink --url --default-stream  --stream-url https://www.nbcnews.com/now?icid=now_hp_header best)\n")
+    youtube_url = "https://www.youtube.com/watch?v=EeQnkxY9QFs"
+    dailymotion_url = "https://www.dailymotion.com/video/x82pp99"
+    telemundo_url = "https://www.youtube.com/@TelemundoEntretenimiento/live"
+    nbcnews_url = "https://www.nbcnews.com/now?icid=now_hp_header"
+    file_path = "./BLINK182.m3u"
 
-def main():
-    run_command()
-    install_dependency()
-    install_yt_dlp()
-    install_youtube_dl()
-    install_streamlink()
-    get_lista4_m3u8()
+    if os.path.exists(file_path):
+        logger.error(f"File already exists: {file_path}")
+        return
     
-if __name__ == '__main__':
-    main()
+    install_dependencies()
+    
+    try:
+        with open(file_path, "w") as f:
+            write_m3u_header(f)
+            youtube_title = run_command(f"yt-dlp --get-title {youtube_url}")[0]
+            if not youtube_title:
+                raise Exception(f"Failed to get title for url: {youtube_url}")
+            youtube_thumbnail = run_command(f"yt-dlp --get-thumbnail {youtube_url}")[0]
+            if not youtube_thumbnail:
+                raise Exception(f"Failed to get thumbnail for url: {youtube_url}
