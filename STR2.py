ee0c08ef7a94df0d4e27cae79aaacdee03207a58
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 08:14:20 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -23,18 +23,18 @@ for i in range(1, 5):
     Data = [item.text for item in soup.find_all("span", class_="item-date")]
 
     for title, link in zip(video_titles, video_links):
-    now = datetime.datetime.now()
-    timestamp = now.strftime("%m%d%H%M%S")
-    video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
-    item = soup.find("a", class_="item", href=link)
-    try:
-        image_url = item["style"].split("url(")[1].split(")")[0]
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%m%d%H%M%S")
+        video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
+        item = soup.find("a", class_="item", href=link)
+        try:
+         image_url = item["style"].split("url(")[1].split(")")[0]
     except Exception as e:
-        print(f"Error: {e}")
-        image_url = "https://cdn.iol.pt/img/logostvi/branco/tviplayer.png"
+         print(f"Error: {e}")
+         image_url = "https://cdn.iol.pt/img/logostvi/branco/tviplayer.png"
     if video_url:
-        m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
-        m3u8_file.write("\n")
+         m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
+         m3u8_file.write("\n")
 
 
 time.sleep(12)
