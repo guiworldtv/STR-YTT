1fbca24d76d7453ee25ac226db45b8a0c109a8ec
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 03:27:33 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import streamlink
-import time
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
@@ -11,21 +10,21 @@ headers = {
 m3u8_file = open("lista2str2.m3u", "w")
 
 url = "https://tviplayer.iol.pt/"
+
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
-video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", href="/direto/TVI")]
-image_urls = [item["style"].split("url(")[1].split(")")[0] for item in soup.find_all("div", class_="programCover")]
-video_titles = [item.text for item in soup.find_all("div", class_="titlePrograma")]
+video_link = soup.find("a", href="/direto/TVI")["href"]
+video_link = f"https://tviplayer.iol.pt{video_link}"
 
-for title, link, image in zip(video_titles, video_links, image_urls):
-    now = datetime.datetime.now()
-    timestamp = now.strftime("%m%d%H%M%S")
-    video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
-    if video_url:
-        m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image}\",{title}\n{video_url}\n")
-        m3u8_file.write("\n")
+video_url = streamlink.streams(video_link)["best"].url if streamlink.streams(video_link) else None
 
-time.sleep(12)
+programs = soup.find_all("div", class_="programCover")
+program_names = [program.find_parent("div", class_="program").find("div", class_="titlePrograma").text for program in programs]
+program_images = [program["style"].split("url(")[1].split(")")[0] for program in programs]
+
+for name, image in zip(program_names, program_images):
+    m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image}\",{name}\n{video_url}\n")
+    m3u8_file.write("\n")
 
 m3u8_file.close()
