a479bd7683bd7f33dd2e23ea6e5cc90a116f0b65
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 08:10:21 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 import streamlink
+import time
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
@@ -8,34 +10,33 @@ headers = {
 
 m3u8_file = open("lista2str2.m3u", "w")
 
-# Request the main page
-url = "https://tviplayer.iol.pt/"
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.content, "html.parser")
 
-# Find all program links
-program_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", href=lambda x: x and x.startswith("/programa"))]
 
-# Visit each program page
-for program_link in program_links:
-    program_response = requests.get(program_link, headers=headers)
-    program_soup = BeautifulSoup(program_response.content, "html.parser")
-    
-    # Find the first video link
-    video_link = f"https://tviplayer.iol.pt{program_soup.find('a', class_='item')['href']}"
-    
-    # Get video details
-    video_url = streamlink.streams(video_link)["best"].url if streamlink.streams(video_link) else None
+for i in range(1, 5):
+    url = f"https://tviplayer.iol.pt/videos/ultimos/{i}/canal:"
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    video_titles = [item.text for item in soup.find_all("span", class_="item-title")]
+    video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", class_="item")]
+    Data = [item.text for item in soup.find_all("span", class_="item-date")]
+
+    for title, link in zip(video_titles, video_links):
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%m%d%H%M%S")
+    video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
+    item = soup.find("a", class_="item", href=link)
     try:
-        image_url = program_soup.find('a', class_='item')['style'].split("url(")[1].split(")")[0]
+        image_url = item["style"].split("url(")[1].split(")")[0]
     except Exception as e:
         print(f"Error: {e}")
         image_url = "https://cdn.iol.pt/img/logostvi/branco/tviplayer.png"
-    title = program_soup.find('span', class_='item-program-title').text
-    
-    # Write to the m3u8 file
     if video_url:
         m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
         m3u8_file.write("\n")
+
+
+time.sleep(12)
 
 m3u8_file.close()
