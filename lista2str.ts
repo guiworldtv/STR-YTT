a2e089edf84047046c75051ea14ad6257eeb071a
GitHub Action <action@github.com>
2023-02-09 18:21:13 +0000

    links atualizados

--- lista2str.ts
+++ lista2str.ts
@@ -3,12 +3,10 @@
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000
-#EXTINF:-1 tvg-id="🔴 La Casa de los Famosos 3: EN VIVO 24/7 🔴 | Telemundo Entretenimiento 2023-02-09 18:01" group-title="Reality Show'S Live" group-title="CNN's" tvg-logo="https://i.ytimg.com/vi/EeQnkxY9QFs/maxresdefault.jpg", YOUTUBEAOVIVO - 🔴 La Casa de los Famosos 3: EN VIVO 24/7 🔴 | Telemundo Entretenimiento 2023-02-09 18:01
+#EXTINF:-1 tvg-id="🔴 La Casa de los Famosos 3: EN VIVO 24/7 🔴 | Telemundo Entretenimiento 2023-02-09 18:19" group-title="Reality Show'S Live" group-title="CNN's" tvg-logo="https://i.ytimg.com/vi/EeQnkxY9QFs/maxresdefault.jpg", YOUTUBEAOVIVO - 🔴 La Casa de los Famosos 3: EN VIVO 24/7 🔴 | Telemundo Entretenimiento 2023-02-09 18:19
 [cli][info] Found matching plugin youtube for URL https://www.youtube.com/@TelemundoEntretenimiento/live
 [cli][info] Available streams: 144p (worst), 240p, 360p, 480p, 720p, 1080p (best)
 [cli][info] Opening stream: 1080p (hls)
 [cli][info] Writing output to
 /home/runner/work/STR-YT/STR-YT/BBUSAMEXICO.ts
-[stream.hls][info] Stopping stream early after 120
-[cli][info] Stream ended
-[cli][info] Closing currently open stream...
+[cli][error] File BBUSAMEXICO.ts already exists, use --force to overwrite it.
