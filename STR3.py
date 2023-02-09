d446aa6b6bfe102de3965a1685b7477f42384204
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-09 12:54:25 -0300

    Update STR3.py

--- STR3.py
+++ STR3.py
@@ -3,23 +3,15 @@ from bs4 import BeautifulSoup
 import datetime
 import streamlink
 
-import streamlink.compat
-streamlink.compat.monkeypatch()
-streamlink.set_option("hls-segment-threads", 1)
-streamlink.set_option("http-ssl-verify", False)
-streamlink.set_option("http-no-ssl-verify", True)
-streamlink.set_option("http-headers", headers)
-streamlink.set_option("hls-live-edge", 9999999)
-streamlink.set_option("hls-timeout", 9999999)
-streamlink.set_option("hls-start-offset", 9999999)
-streamlink.set_option("http-user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
-streamlink.set_loglevel("error")
-streamlink.set_logoutput(None)
-
-
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
+
+streamlink.set_option("http-ssl-verify", False)
+streamlink.set_option("http-headers", headers)
+streamlink.set_option("http-user-agent", headers["User-Agent"])
+streamlink.set_loglevel("error")
+streamlink.set_logoutput(None)
 
 m3u8_file = open("lista3str3.m3u", "w")
 
@@ -32,18 +24,15 @@ video_titles = [item.text for item in soup.find_all("yt-formatted-string", class
 video_images = [item["src"] for item in soup.find_all("img", class_="yt-core-image")]
 video_links = [f"https://www.youtube.com{item['href']}" for item in soup.find_all("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")]
 
-
 for title, link in zip(video_titles, video_links):
-    now = datetime.datetime.now()
-    timestamp = now.strftime("%m%d%H%M%S")
     video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
-if video_url:
-    image_url = f"https://i.ytimg.com/vi/{link.split('watch?v=')[1]}/hqdefault.jpg"
-    m3u8_file.write(f"#EXTINF:-1 tvg-group=\"YOUTOBA\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
-    m3u8_file.write("\n")
-else:
-    print(f"Não foi possível acessar o vídeo: {title}")
-
-    
+    if video_url:
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%m%d%H%M%S")
+        image_url = f"https://i.ytimg.com/vi/{link.split('watch?v=')[1]}/hqdefault.jpg"
+        m3u8_file.write(f"#EXTINF:-1 tvg-group=\"YOUTOBA\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
+        m3u8_file.write("\n")
+    else:
+        print(f"Não foi possível acessar o vídeo: {title}")
 
 m3u8_file.close()
