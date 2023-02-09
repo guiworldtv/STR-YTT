3e33d018f55b2df9128ee76df27757a98bf77138
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-09 14:50:16 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -1,41 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-import datetime
-import streamlink
-import time
+import subprocess
+import os
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-}
+# Instalando yt-dlp
+subprocess.run(['sudo', 'wget', 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp', '-O', '/usr/local/bin/yt-dlp'])
+subprocess.run(['sudo', 'chmod', 'a+rx', '/usr/local/bin/yt-dlp'])
 
-m3u8_file = open("lista2str2.m3u", "w")
+# Instalando youtube-dl
+subprocess.run(['sudo', 'wget', 'https://yt-dl.org/downloads/latest/youtube-dl', '-O', '/usr/local/bin/youtube-dl'])
+subprocess.run(['sudo', 'chmod', 'a+rx', '/usr/local/bin/youtube-dl'])
+subprocess.run(['youtube-dl', '-U'])
 
+# Instalando streamlink
+subprocess.run(['pip', 'install', '--user', '--upgrade', 'streamlink'])
 
-
-for i in range(1, 6):
-    url = f"https://tviplayer.iol.pt/videos/ultimos/{i}/canal:"
-
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    video_titles = [item.text for item in soup.find_all("span", class_="item-title")]
-    video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", class_="item")]
-    Data = [item.text for item in soup.find_all("span", class_="item-date")]
-
-    for title, link in zip(video_titles, video_links):
-        now = datetime.datetime.now()
-        timestamp = now.strftime("%m%d%H%M%S")
-        video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
-        item = soup.find("a", class_="item", href=link)
-        try:
-            image_url = item["style"].split("url(")[1].split(")")[0]
-        except Exception as e:
-            print(f"Error: {e}")
-            image_url = "https://cdn.iol.pt/img/logostvi/branco/tviplayer.png"
-        if video_url:
-            m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image_url}\",{title}\n{video_url}\n")
-            m3u8_file.write("\n")
-
-time.sleep(12)
-
-m3u8_file.close()
+# Get LISTA4.m3u8
+with open('./lista2str.m3u', 'w') as f:
+    f.write("#EXTM3U\n")
+    f.write("#EXT-X-VERSION:3\n")
+    f.write("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n")
+    title = subprocess.run(['yt-dlp', '--get-title', 'https://www.youtube.com/c/GuilhermeMartinsTV/videos'], stdout=subprocess.PIPE)
+    title = title.stdout.decode().strip()
+    thumbnail = subprocess.run(['yt-dlp', '--get-thumbnail', 'https://www.youtube.com/c/GuilhermeMartinsTV/videos'], stdout=subprocess.PIPE)
+    thumbnail = thumbnail.stdout.decode().strip()
+    url = subprocess.run(['yt-dlp', '--get-url', 'https://www.youtube.com/c/GuilhermeMartinsTV/videos'], stdout=subprocess.PIPE)
+    url = url.stdout.decode().strip()
+    f.write(f"#EXTINF:-1 tvg-id='{title}' tvg-logo='{thumbnail}',{title}\n")
+    f.write(f"{url}\n")
