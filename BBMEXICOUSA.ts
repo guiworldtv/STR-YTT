c5625914237a57e4ab914fcacf857bb54e26c782
GitHub Action <action@github.com>
2023-02-11 16:30:22 +0000

    links atualizados

--- BBMEXICOUSA.ts
+++ BBMEXICOUSA.ts
@@ -3,16 +3,16 @@
 [cli][info] Available streams: 144p (worst), 240p, 360p, 480p, 720p, 1080p (best)
 [cli][info] Opening stream: 1080p (hls)
 [cli][info] Writing output to
-/home/runner/work/STR-YT/STR-YT/1102_162629_BBMEXICOUSA_-2023.ts
+/GRAVADOS/1102_163012_BBMEXICOUSA_-2023.ts
 [stream.hls][info] Stopping stream early after 15
-[cli][info] Stream ended
+error: Failed to open output: /GRAVADOS/1102_163012_BBMEXICOUSA_-2023.ts ([Errno 13] Permission denied: '/GRAVADOS')
 [cli][info] Closing currently open stream...
 [cli][info] Found matching plugin cbsnews for URL https://www.cbsnews.com/live/
 [cli][info] Available streams: 180p (worst), 360p_alt, 360p, 540p, 720p_alt, 720p (best)
 [cli][info] Opening stream: 720p (hls)
 [cli][info] Writing output to
-/home/runner/work/STR-YT/STR-YT/1102_162631_CBSNEWS_-2023.ts
+/GRAVADOS/1102_163013_CBSNEWS_-2023.ts
 [stream.hls][info] Stopping stream early after 15
-[cli][info] Stream ended
+error: Failed to open output: /GRAVADOS/1102_163013_CBSNEWS_-2023.ts ([Errno 13] Permission denied: '/GRAVADOS')
 [cli][info] Closing currently open stream...
          
