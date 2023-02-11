1dadf25fb1096169a3af7c2070d115cc3e116762
GitHub Action <action@github.com>
2023-02-11 16:19:30 +0000

    links atualizados

--- BBMEXICOUSA.ts
+++ BBMEXICOUSA.ts
@@ -3,7 +3,7 @@
 [cli][info] Available streams: 144p (worst), 240p, 360p, 480p, 720p, 1080p (best)
 [cli][info] Opening stream: 1080p (hls)
 [cli][info] Writing output to
-/home/runner/work/STR-YT/STR-YT/BBMEXICOUSA-2023-02-11-16-14-51.ts
+/home/runner/work/STR-YT/STR-YT/BBMEXICOUSA-1102--16-19-17-2023-.ts
 [stream.hls][info] Stopping stream early after 15
 [cli][info] Stream ended
 [cli][info] Closing currently open stream...
@@ -11,7 +11,7 @@
 [cli][info] Available streams: 180p (worst), 360p_alt, 360p, 540p, 720p_alt, 720p (best)
 [cli][info] Opening stream: 720p (hls)
 [cli][info] Writing output to
-/home/runner/work/STR-YT/STR-YT/CBSNEWS-2023-02-11-16-14-52.ts
+/home/runner/work/STR-YT/STR-YT/CBSNEWS-1102--16-19-19-2023-.ts
 [stream.hls][info] Stopping stream early after 15
 [cli][info] Stream ended
 [cli][info] Closing currently open stream...
