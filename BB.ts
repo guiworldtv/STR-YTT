9e8b1ca985a3445e5fda312f1b3da047dd33f41d
GitHub Action <action@github.com>
2023-02-11 14:37:13 +0000

    links atualizados

---
+++ BB.ts
@@ -0,0 +1,13 @@
+
+
+
+
+[cli][info] Found matching plugin youtube for URL https://www.youtube.com/@TelemundoEntretenimiento/live
+[cli][info] Available streams: 144p (worst), 240p, 360p, 480p, 720p, 1080p (best)
+[cli][info] Opening stream: 1080p (hls)
+[cli][info] Writing output to
+/home/runner/work/STR-YT/STR-YT/BB.ts
+[cli][error] File BB.ts already exists, use --force to overwrite it.
+
+
+EOL             
