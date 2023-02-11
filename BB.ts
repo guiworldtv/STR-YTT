2116ed2f064d809e423d3fa1dc6644f271f6a592
GitHub Action <action@github.com>
2023-02-11 14:34:01 +0000

    links atualizados

---
+++ BB.ts
@@ -0,0 +1,11 @@
+
+
+
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000
+#EXTINF:-1 tvg-id="🔴 La Casa de los Famosos 3: EN VIVO 24/7 🔴 | Telemundo Entretenimiento 2023-02-11 14:33" group-title="Reality Show'S Live" group-title="CNN's" tvg-logo="https://i.ytimg.com/vi/EeQnkxY9QFs/maxresdefault.jpg", YOUTUBEAOVIVO - 🔴 La Casa de los Famosos 3: EN VIVO 24/7 🔴 | Telemundo Entretenimiento 2023-02-11 14:33
+https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1676147640/ei/WKfnY53EFeGoir4P3ey2-A0/ip/40.77.50.160/id/EeQnkxY9QFs.1/itag/96/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D137/hls_chunk_host/rr5---sn-q4flrn7y.googlevideo.com/playlist_duration/30/manifest_duration/30/gcr/us/vprv/1/playlist_type/DVR/initcwndbps/780000/mh/-1/mm/44/mn/sn-q4flrn7y/ms/lva/mv/m/mvi/5/pl/17/dover/11/pacing/0/keepalive/yes/fexp/24007246/mt/1676125721/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,gcr,vprv,playlist_type/sig/AOq0QJ8wRQIgR1qRUZbkNZtDW0JxHmc9TlCoCcN_BIsRcGjkSjsHIY0CIQCAI-6cKfzNib2pCr46wnxIv25UjexJhfJPpIokzoBjTQ%3D%3D/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRQIgecEXZePMxxNmg85Zv9y0zA51k66VaA-o3Cv2ucu6gUcCIQDDPp2LPRtVYCiIeYJv9dvZppn4kO7lgJBEJ0VvOeX6Mg%3D%3D/playlist/index.m3u8
+
+
+EOL             
