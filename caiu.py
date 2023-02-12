8ca9c08eaf6efe5383b100d550d699fa68902578
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-12 08:28:09 -0300

    Update caiu.py

--- caiu.py
+++ caiu.py
@@ -1,3 +1,81 @@
+# Obter informações do Codespaces Secrets
+api_key = os.environ.get('APIGOOGLEDRIVE')
+
+# Autenticação do usuário
+creds = None
+if api_key:
+    creds = api_key
+else:
+    creds, _ = google.auth.default()
+
+service = build("drive", "v3", credentials=creds)
+ import sys
+import pytz
+import datetime
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+try:
+    import streamlink
+except ImportError:
+    os.system("pip install --user --upgrade streamlink")
+
+try:
+    import youtube_dl
+    import requests
+    import beautifulsoup4
+    import fastapi
+    import uvicorn
+    import schedule
+    import selenium
+    import google.auth
+    from googleapiclient.discovery import build
+
+except ImportError:
+    os.system("pip install youtube-dl requests beautifulsoup4 fastapi uvicorn schedule selenium datetime google-auth google-api-python-client")
+
+def create_folder(folder_name):
+    """Cria uma nova pasta no Google Drive"""
+    try:
+        # Autenticação do usuário
+        creds, _ = google.auth.default()
+        service = build("drive", "v3", credentials=creds)
+        
+        file_metadata = {
+            'name': folder_name,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        folder = service.files().create(body=file_metadata, fields='id').execute()
+        print(F'Pasta criada com o ID: {folder.get("id")}')
+        return folder.get('id')
+    except HttpError as error:
+        print(F'An error occurred while creating the folder: {error}')
+        return None
+
+def upload_to_folder(file_path, file_name, folder_id):
+    """Faz upload do arquivo para a pasta especificada"""
+    try:
+        # Autenticação do usuário
+        creds, _ = google.auth.default()
+        service = build("drive", "v3", credentials=creds)
+
+        file_metadata = {'name': file_name, 'parents':[folder_id]}
+        media = MediaFileUpload(file_path,
+                                mimetype='video/MP2T')
+        file = service.files().create(body=file_metadata, media_body=media,
+                                      fields='id').execute()
+        print(F'File ID: {file.get("id")}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+    return file
+
+def main():
+    # Obtenha a hora atual no fuso horário de São Paulo
+    tz = pytz.timezone("America/Sao_Paulo")
+    
+    
 import os
 import sys
 import pytz
