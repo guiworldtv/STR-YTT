e3094975b875badb2543f517f4e41bf15660f2dc
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-12 08:39:56 -0300

    Create caiu.py

--- caiu.py
+++ caiu.py
@@ -1,22 +1,9 @@
-# Obter informações do Codespaces Secrets
-api_key = os.environ.get('APIGOOGLEDRIVE')
-
-# Autenticação do usuário
-creds = None
-if api_key:
-    creds = api_key
-else:
-    creds, _ = google.auth.default()
-
-service = build("drive", "v3", credentials=creds)
-
+import os
 import sys
 import pytz
 import datetime
-import google.auth
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
+# Instala bibliotecas necessárias se não estiverem presentes
 try:
     import streamlink
 except ImportError:
@@ -30,19 +17,21 @@ try:
     import uvicorn
     import schedule
     import selenium
-    import google.auth
-    from googleapiclient.discovery import build
-
 except ImportError:
-    os.system("pip install youtube-dl requests beautifulsoup4 fastapi uvicorn schedule selenium datetime google-auth google-api-python-client")
+    os.system("pip install youtube-dl requests beautifulsoup4 fastapi uvicorn schedule selenium datetime")
+
+# Autenticação do usuário com as credenciais do Codespaces Secrets
+api_key = os.environ.get('APIGOOGLEDRIVE')
+creds = None
+if api_key:
+    creds = api_key
+else:
+    creds, _ = google.auth.default()
+service = build("drive", "v3", credentials=creds)
 
 def create_folder(folder_name):
     """Cria uma nova pasta no Google Drive"""
     try:
-        # Autenticação do usuário
-        creds, _ = google.auth.default()
-        service = build("drive", "v3", credentials=creds)
-        
         file_metadata = {
             'name': folder_name,
             'mimeType': 'application/vnd.google-apps.folder'
@@ -51,62 +40,28 @@ def create_folder(folder_name):
         print(F'Pasta criada com o ID: {folder.get("id")}')
         return folder.get('id')
     except HttpError as error:
-        print(F'An error occurred while creating the folder: {error}')
+        print(F'Ocorreu um erro ao criar a pasta: {error}')
         return None
 
 def upload_to_folder(file_path, file_name, folder_id):
     """Faz upload do arquivo para a pasta especificada"""
     try:
-        # Autenticação do usuário
-        creds, _ = google.auth.default()
-        service = build("drive", "v3", credentials=creds)
-
         file_metadata = {'name': file_name, 'parents':[folder_id]}
-        media = MediaFileUpload(file_path,
-                                mimetype='video/MP2T')
+        media = MediaFileUpload(file_path, mimetype='video/MP2T')
         file = service.files().create(body=file_metadata, media_body=media,
                                       fields='id').execute()
         print(F'File ID: {file.get("id")}')
     except HttpError as error:
-        print(F'An error occurred: {error}')
+        print(F'Ocorreu um erro: {error}')
         file = None
     return file
 
 def main():
     # Obtenha a hora atual no fuso horário de São Paulo
     tz = pytz.timezone("America/Sao_Paulo")
-    
-    
-import os
-import sys
-import pytz
-import datetime
-try:
-    import streamlink
-except ImportError:
-    os.system("pip install --user --upgrade streamlink")
-
-try:
-    import youtube_dl
-    import requests
-    import beautifulsoup4
-    import fastapi
-    import uvicorn
-    import schedule
-    import selenium
-
-except ImportError:
-    os.system("pip install youtube-dl requests beautifulsoup4 fastapi uvicorn schedule selenium datetime")
-
-
-def main():
-    os.system("touch ./BBMEXICOUSA.ts")
-    os.system("sudo cat >./BBMEXICOUSA.ts <<EOL")
-
-    # Obtenha a hora atual no fuso horário de São Paulo
-    tz = pytz.timezone("America/Sao_Paulo")
     hora_atual = datetime.datetime.now(tz).strftime("%d%m_%H%M%S")
 
+    # Grava streams usando streamlink
     streamlink_command_1 = "streamlink --force --hls-duration 00:04:00 --output "
     streamlink_command_1 += f"\"GRAVADOS/{hora_atual}_CBSTELEMUNDO_.ts\" https://www.youtube.com/@TelemundoEntretenimiento/live best"
     os.system(streamlink_command_1)
