0bec8f69cc575dcd3854333947e19cebe52135e9
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-19 03:11:17 -0300

    Update STR2.py

--- STR2.py
+++ STR2.py
@@ -18,18 +18,17 @@ for i in range(1, 6):
 
     video_titles = [item.text for item in soup.find_all("span", class_="item-title")]
     video_links = [f"https://tviplayer.iol.pt{item['href']}" for item in soup.find_all("a", class_="item")]
-    video_thumbnails = [item["style"].split("url(")[1].split(")")[0] for item in soup.find_all("a", class_="item")]
-    video_dates = [item.text for item in soup.find_all("span", class_="item-date")]
+    subtitles = [item.text for item in soup.find_all("span", class_="item-program-title")]
+    images = [item['style'].split('url(')[1].split(')')[0] for item in soup.select("a.item")]
 
-    for title, link, thumbnail, date in zip(video_titles, video_links, video_thumbnails, video_dates):
+    for title, link, subtitle, image_url in zip(video_titles, video_links, subtitles, images):
         now = datetime.datetime.now()
         timestamp = now.strftime("%m%d%H%M%S")
         video_url = streamlink.streams(link)["best"].url if streamlink.streams(link) else None
+
         if video_url:
-            m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{thumbnail}\",{title}\n{video_url}\n")
+            m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image_url}\",{subtitle} - {title}\n{video_url}\n")
             m3u8_file.write("\n")
-        else:
-            print(f"Deu erro para baixar {title} ({link})")
 
 time.sleep(13)
 
